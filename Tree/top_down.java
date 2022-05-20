70878186759f69c2ebea1a0b89515b56c3e4acd6
hlin863 <55798830+hlin863@users.noreply.github.com>
2022-05-20 17:20:12 +0100

    Update top_down.java
    I have implemented two queues to track the level and the node to ensure a top-down representation of the tree structure.

--- Tree/top_down.java
+++ Tree/top_down.java
@@ -14,6 +14,7 @@ class Node {
 }
 
 class Solution {
+
 	/* 
     
     class Node 
@@ -21,74 +22,29 @@ class Solution {
     	Node left;
     	Node right;
 	*/
-	public static void topView(Node root) {
-        ArrayList<ArrayList<Node>> a = new ArrayList<>();
+	public static void levelOrder(Node root) {
+      
+        // performs inorder traversal.
+        Queue<Node> q = new LinkedList<Node>();
 
-        int numberOfLevels = findNumberOfLevels(root); // finds the number of levels in the tree
+        q.add(root);
 
-        System.out.println("Number of levels: " + numberOfLevels);
+        while (!q.isEmpty()){
 
-        for (int i = 0; i < numberOfLevels; i++) {
-            a.add(new ArrayList<Node>()); // added a placeholder node to the arraylist
-        }
+            Node temp = q.poll();
 
-        a = topDownView(root, a, numberOfLevels); // calls the topDownView method
+            System.out.print(temp.data + " ");
 
-    }
-
-    public static ArrayList<ArrayList<Node>> topDownView(Node root, ArrayList<Node> a, int numberOfLevels) {
-        if (root == null) {
-            return a;
-        }
-
-        // TO-DO write a topdown approach method to change the tree structure into a 2D arrayList. 
-
-        a = topDownView(root.left, a, numberOfLevels); // calls the topDownView method on the left child
-        a = topDownView(root.right, a, numberOfLevels); // calls the topDownView method on the right child
-
-        return a;
-    }
-
-    public static int findNumberOfLevels(Node root){
-        // function to find the number of levels in the tree;
-        if (root == null) {
-            return 0;
-        } else {
-            if (findNumberOfLevels(root.left) > findNumberOfLevels(root.right)) {
-                return findNumberOfLevels(root.left) + 1;
-            } else {
-                return findNumberOfLevels(root.right) + 1;
+            if (temp.left != null){
+                q.add(temp.left);
             }
+
+            if (temp.right != null){
+                q.add(temp.right);
+            }
+
         }
-
+      
     }
-
 
 	public static Node insert(Node root, int data) {
-        if(root == null) {
-            return new Node(data);
-        } else {
-            Node cur;
-            if(data <= root.data) {
-                cur = insert(root.left, data);
-                root.left = cur;
-            } else {
-                cur = insert(root.right, data);
-                root.right = cur;
-            }
-            return root;
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int t = scan.nextInt();
-        Node root = null;
-        while(t-- > 0) {
-            int data = scan.nextInt();
-            root = insert(root, data);
-        }
-        scan.close();
-        topView(root);
-    }	
-}
