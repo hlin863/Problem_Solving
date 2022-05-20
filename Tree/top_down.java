f947cf6cc9fb04af44444f0b79ef18194dd388f4
hlin863 <55798830+hlin863@users.noreply.github.com>
2022-05-20 17:20:59 +0100

    Included entire file

--- Tree/top_down.java
+++ Tree/top_down.java
@@ -48,3 +48,30 @@ class Solution {
     }
 
 	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        levelOrder(root);
+    }	
+}
