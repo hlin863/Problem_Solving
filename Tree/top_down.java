5cc1bdc71dc1c37364bc9e04755de0a94133814f
hlin863 <55798830+hlin863@users.noreply.github.com>
2022-05-20 10:43:30 +0100

    Update top_down.java
    I have identified an alternative approach to convert the tree into a two-dimensional ArrayList structure.

--- Tree/top_down.java
+++ Tree/top_down.java
@@ -14,7 +14,6 @@ class Node {
 }
 
 class Solution {
-
 	/* 
     
     class Node 
@@ -23,35 +22,45 @@ class Solution {
     	Node right;
 	*/
 	public static void topView(Node root) {
-      
-        //   intialise a queue
-        Queue<Node> q = new LinkedList<Node>();
+        ArrayList<ArrayList<Node>> a = new ArrayList<>();
 
-        if (root == null) {
-            return; // if the queue is empty, return
-        } else {
-            q.add(root); // adds the root into the queue. 
+        int numberOfLevels = findNumberOfLevels(root); // finds the number of levels in the tree
+
+        System.out.println("Number of levels: " + numberOfLevels);
+
+        for (int i = 0; i < numberOfLevels; i++) {
+            a.add(new ArrayList<Node>()); // added a placeholder node to the arraylist
         }
 
-        levelOrder(root); // prints the level order traversal of the tree.
-      
+        a = topDownView(root, a, numberOfLevels); // calls the topDownView method
+
     }
 
-    // write a function to list all the nodes at a given level in an array
-    public static void levelOrder(Node root) {
-        // Write your code here
-        Queue<Node> q = new LinkedList<Node>();
-        q.add(root);
-        while (!q.isEmpty()) {
-            Node temp = q.remove();
-            System.out.print(temp.data + " ");
-            if (temp.left != null) {
-                q.add(temp.left);
-            }
-            if (temp.right != null) {
-                q.add(temp.right);
+    public static ArrayList<ArrayList<Node>> topDownView(Node root, ArrayList<Node> a, int numberOfLevels) {
+        if (root == null) {
+            return a;
+        }
+
+        // TO-DO write a topdown approach method to change the tree structure into a 2D arrayList. 
+
+        a = topDownView(root.left, a, numberOfLevels); // calls the topDownView method on the left child
+        a = topDownView(root.right, a, numberOfLevels); // calls the topDownView method on the right child
+
+        return a;
+    }
+
+    public static int findNumberOfLevels(Node root){
+        // function to find the number of levels in the tree;
+        if (root == null) {
+            return 0;
+        } else {
+            if (findNumberOfLevels(root.left) > findNumberOfLevels(root.right)) {
+                return findNumberOfLevels(root.left) + 1;
+            } else {
+                return findNumberOfLevels(root.right) + 1;
             }
         }
+
     }
 
 
